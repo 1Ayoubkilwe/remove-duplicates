2f9fb6004c034327d104755bcf7c41b294a4e987
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 16:40:58 +0300

    answers

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+  }
+  
+  console.log(removeDuplicates([1, 0, 1, 0])); // [1, 0]
+  console.log(removeDuplicates(["New", "York", "City"])); // ["New", "York", "City"]
+  console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"])); // ["Hassan", "Aisha"]
+  
